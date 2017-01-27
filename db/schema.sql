d7c223891287200581e5cf9fdbfa5e3c53a6399e
poornima sewak <poornima.mathur.20@gmail.com>
2017-01-27 17:09:39 -0600

    final code

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+id INTEGER(11) NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(250) NOT NULL,
+devoured boolean NOT NULL DEFAULT FALSE,
+date TIMESTAMP NOT NULL,
+PRIMARY KEY (id)
+);
